3450936f3aa3b3c635450bbb2bd1c917091c741a
Fukuball Lin <fukuball@gmail.com>
2013-06-21 00:25:58 +0800

    fukuball

--- fuku-view-page/home-default.php
+++ fuku-view-page/home-default.php
@@ -18,5 +18,15 @@
          <h4>About</h4>
          <p>CTO of iNDIEVOX 獨立音樂網, the largest indie music web site in Taiwan. / I'm also a happy guitar player.</p>
       </li>
+      <li>
+         <h4>Links</h4>
+      </li>
+      <li>
+         <h4>Team</h4>
+         <a class="team-link track" data-action="view team" data-from="profile sidebar" href="/team/indievox">
+            <span class="team-avatar"><img alt="Indievox-touch-icon" height="22" src="https://coderwall-assets-0.s3.amazonaws.com/uploads/team/avatar/51ada2cfcf8fa5000b000001/indievox-touch-icon.png" width="22"></span>
+            <div itemprop="affiliation">iNDIEVOX</div>
+         </a>
+      </li>
    </ul>
 </section>
