e73d61e86e0f3355da0b072c928d961c0fb68240
Fukuball Lin <fukuball@gmail.com>
2013-06-20 17:10:22 +0800

    fukuball

---
+++ fuku-config/app-setter.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * app-setter.php initialize application settings
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-config/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) . '/error.log');
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+session_start();
+
+// http or https
+$host_protocol = 'http';
+if ($_SERVER["HTTPS"] == "on") {
+   $host_protocol = 'https';
+}
+require_once dirname(__FILE__)."/system_environment.php";
+
+// library
+require_once SITE_ROOT."/fuku-class/fuku.inc";
+?>
