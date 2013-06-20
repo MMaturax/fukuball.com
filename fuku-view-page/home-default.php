a282e64d70a088da4bf0ac483606370ee493fc74
Fukuball Lin <fukuball@gmail.com>
2013-06-21 00:58:33 +0800

    fukuball

--- fuku-view-page/home-default.php
+++ fuku-view-page/home-default.php
@@ -20,16 +20,25 @@
       </li>
       <li>
          <h4 class="m-t-0">Links</h4>
-         <a type="button" class="btn btn-small" href="https://twitter.com/fukuball">
+         <a title="fukuball" type="button" class="btn btn-small" href="https://twitter.com/fukuball">
             <i class="icon-twitter icon-large"></i>
          </a>
-         <button type="button" class="btn btn-small" href="https://github.com/fukuball">
+         <a type="button" class="btn btn-small" href="https://github.com/fukuball">
             <i class="icon-github-alt icon-large"></i>
-         </button>
+         </a>
+         <a type="button" class="btn btn-small" href="https://facebook.com/fukuball">
+            <i class="icon-facebook icon-large"></i>
+         </a>
+         <a type="button" class="btn btn-small" href="https://linkedin.com/in/fukuball">
+            <i class="icon-linkedin icon-large"></i>
+         </a>
+         <a type="button" class="btn btn-small" href="https://coderwall.com/fukuball">
+            <i class="icon-rss icon-large"></i>
+         </a>
       </li>
       <li>
          <h4 class="m-t-0">Team</h4>
-         <a class="team-link track" href="http://www.indievox.com">
+         <a title="iNDIEVOX" class="team-link track" href="http://www.indievox.com">
             <span class="team-avatar">
                <img src="/public/image/fukuball-touch-icon-72x72.png" target="_blank" height="22" width="22">
             </span>
