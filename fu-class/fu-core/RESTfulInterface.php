3c62aa51f3599b8c8065fde7807a99d9da3d42b5
Fukuball Lin <fukuball@gmail.com>
2013-06-20 14:36:26 +0800

    rest

---
+++ fu-class/fu-core/RESTfulInterface.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * RESTfulInterface.php is the interface
+ * to describe rest methods
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fu-class/fu-core/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+
+/**
+ * RESTfulInterface is the interface
+ * to describe rest methods
+ *
+ * An example of a RESTfulInterface is:
+ *
+ * <code>
+ *  # This will done by rest request
+ * </code>
+ *
+ * @category PHP
+ * @package  /iv-class/rest/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+interface RESTfulInterface
+{
+
+    /**
+     * Dispatch get actions
+     *
+     * @param array $segments Method segments indicate action and resource
+     *
+     * @return void
+     */
+    public function restGet($segments);
+
+    /**
+     * Dispatch put actions
+     *
+     * @param array $segments Method segments indicate action and resource
+     *
+     * @return void
+     */
+    public function restPut($segments);
+
+    /**
+     * Dispatch post actions
+     *
+     * @param array $segments Method segments indicate action and resource
+     *
+     * @return void
+     */
+    public function restPost($segments);
+
+    /**
+     * Dispatch delete actions
+     *
+     * @param array $segments Method segments indicate action and resource
+     *
+     * @return void
+     */
+    public function restDelete($segments);
+
+}// end interface RESTfulInterface
+?>
