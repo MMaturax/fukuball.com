9f2c5f6e6ab8f0b23ceb1a4849e3f1d2e6fceb0a
Fukuball Lin <fukuball@gmail.com>
2013-06-20 23:55:25 +0800

    fukuball

---
+++ fuku-view-component/javascript-include.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * javascript-include.php is head include javascript
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-component/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
---
+++ fuku-view-component/style-include.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * style-include.php is head include style
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-component/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+?>
+<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
