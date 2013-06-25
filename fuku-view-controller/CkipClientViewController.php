ea4a7ee039280489eb9f106ca2f6435fba37bc18
Fukuball Lin <fukuball@gmail.com>
2013-06-26 00:04:39 +0800

    ckip

---
+++ fuku-view-controller/CkipClientViewController.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * CkipClientViewController.php is the controller
+ * to dispatch ckip-client actions with ckip-client view
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-controller/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+
+/**
+ * CkipClientViewController.php is the controller
+ * to dispatch ckip-client actions with ckip-client view
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-controller/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+class CkipClientViewController
+   extends RESTControl
+      implements RESTfulInterface
+{
+
+   /**
+    * Dispatch post actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restPost($segments)
+   {
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new IndievoxErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restPost
+
+   /**
+    * Dispatch get actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restGet($segments)
+   {
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+
+         $page_title = 'Fukuball Lin';
+         $yield_path = '/fuku-view-page/home-default.php';
+         require_once SITE_ROOT.'/fuku-view-layout/full-page-layout.php';
+
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restGet
+
+   /**
+    * Dispatch put actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restPut($segments)
+   {
+
+      $_PUT = array();
+      parse_str(file_get_contents('php://input'), $_PUT);
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new IndievoxErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restPut
+
+
+   /**
+    * Dispatch delete actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restDelete($segments)
+   {
+
+      $_DELETE = array();
+      parse_str(file_get_contents('php://input'), $_DELETE);
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new IndievoxErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restDelete
+
+}
+?>
