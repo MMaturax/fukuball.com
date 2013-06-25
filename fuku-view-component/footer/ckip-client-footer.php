d5ea5415a1b4003588d4be00169589285d35ab2f
Fukuball Lin <fukuball@gmail.com>
2013-06-26 01:42:05 +0800

    ckip

--- fuku-view-component/footer/ckip-client-footer.php
+++ fuku-view-component/footer/ckip-client-footer.php
@@ -15,12 +15,16 @@
 <footer>
    <div class="container">
       <div class="row">
-         <div class="span5">
+         <div class="span5 pull-left">
             <h3>CKIP Client 線上中文斷詞</h3>
             <ul class="footer-links">
                <li><a href="/ckip-client/">線上中文斷詞首頁</a></li>
                <li><a href="/ckip-client/about">關於我們</li>
             </ul>
+         </div>
+         <div class="span5 pull-right">
+            <div id="footer-commercial">
+            </div>
          </div>
       </div>
    </div>
