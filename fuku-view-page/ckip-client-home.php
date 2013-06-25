77ec7a39cffed624e0034e56e78860f4c161e2e2
Fukuball Lin <fukuball@gmail.com>
2013-06-26 02:45:29 +0800

    ckip

--- fuku-view-page/ckip-client-home.php
+++ fuku-view-page/ckip-client-home.php
@@ -13,6 +13,13 @@
  */
 ?>
 <section>
+   <div class="hero-unit">
+      <h1>CKIP Client 線上中文斷詞服務</h1>
+      <p>自然語言處理系統最基本需要讓電腦能夠分辨文本中字詞的意義，才能夠更進一步發展出自然語言處理系統的相關演算法，其中斷詞處理便是一個重要的前置技術， CKIP Client 線上中文斷詞服務網站使用了中研院斷詞系統的 Client 端程式，讓有中文斷詞需求的研究者或程式人員可以專注於開發自己的核心演算法。</p>
+      <p><a href="https://github.com/fukuball/CKIPClient-PHP" class="btn btn-primary btn-large">Fork CKIPClient-PHP »</a></p>
+   </div>
+</section>
+<section>
    <div id="full-width-commercial" class="commercial-box text-center">
       <script type="text/javascript"><!--
       google_ad_client = "ca-pub-9402461567510680";
