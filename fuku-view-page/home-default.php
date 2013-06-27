3165d315a475af52d7723533f7a7a43d87495d5e
Fukuball Lin <fukuball@gmail.com>
2013-06-27 13:48:01 +0800

    fukuball

--- fuku-view-page/home-default.php
+++ fuku-view-page/home-default.php
@@ -12,56 +12,8 @@
  * @link     http://www.fukuball.com
  */
 ?>
-<section class="profile-section container">
-   <ul class="profile-details unstyled">
-      <li>
-         <div class="row m-l-0">
-            <div class="pull-left">
-               <img class="img-rounded" style="width: 50px; height: 50px;" src="/public/image/avatar3.jpg" />
-            </div>
-            <div class="pull-left m-l-10">
-               <h1>fukuball</h1>
-            </div>
-         </div>
-         <div>
-            <span>
-               <i class="icon-location-arrow"></i> Taipei, Taiwan
-            </span>
-            <span class="m-l-20">
-               <i class="icon-user"></i> CTO, iNDIEVOX
-            </span>
-         </div>
-      </li>
-      <li>
-         <h4 class="m-t-0">About</h4>
-         <p>CTO of iNDIEVOX 獨立音樂網, the largest indie music web site in Taiwan. / I'm also a happy guitar player.</p>
-      </li>
-      <li>
-         <h4 class="m-t-0">Links</h4>
-         <a title="fukuball@twitter" type="button" class="btn btn-small" href="https://twitter.com/fukuball" target="_blank">
-            <i class="icon-twitter icon-large"></i>
-         </a>
-         <a title="fukuball@facebook" type="button" class="btn btn-small" href="https://facebook.com/fukuball" target="_blank">
-            <i class="icon-facebook-sign icon-large"></i>
-         </a>
-         <a title="fukuball@linkedin" type="button" class="btn btn-small" href="https://linkedin.com/in/fukuball" target="_blank">
-            <i class="icon-linkedin icon-large"></i>
-         </a>
-         <a title="fukuball@github" type="button" class="btn btn-small" href="https://github.com/fukuball" target="_blank">
-            <i class="icon-github-alt icon-large"></i>
-         </a>
-         <a title="fukuball@coderwall" type="button" class="btn btn-small" href="https://coderwall.com/fukuball" target="_blank">
-            <i class="icon-rss icon-large"></i>
-         </a>
-      </li>
-      <li>
-         <h4 class="m-t-0">Team</h4>
-         <a title="iNDIEVOX" class="team-link track" href="http://www.indievox.com" target="_blank">
-            <span class="team-avatar">
-               <img src="/public/image/fukuball-touch-icon-72x72.png" height="22" width="22">
-            </span>
-            <div>iNDIEVOX</div>
-         </a>
-      </li>
-   </ul>
-</section>
+<div class="m-h-auto" style="width: 330px;">
+   <?php
+   include_once SITE_ROOT.'/fuku-view-component/sidebar/profile-card.php';
+   ?>
+</div>
