0d4c1cd95a6a8656ef2fb16a52e7738a06e746f5
Fukuball Lin <fukuball@gmail.com>
2013-06-27 15:50:13 +0800

    about

--- fuku-view-page/ckip-client-about.php
+++ fuku-view-page/ckip-client-about.php
@@ -53,6 +53,19 @@
          <?php
          include_once SITE_ROOT.'/fuku-view-component/sidebar/profile-card.php';
          ?>
+         <div class="commercial-box text-center" style="width:330px;">
+            <script type="text/javascript"><!--
+            google_ad_client = "ca-pub-9402461567510680";
+            /* 300x250廣告 */
+            google_ad_slot = "6173451467";
+            google_ad_width = 300;
+            google_ad_height = 250;
+            //-->
+            </script>
+            <script type="text/javascript"
+            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+            </script>
+         </div>
       </div>
    </div>
 </section>
