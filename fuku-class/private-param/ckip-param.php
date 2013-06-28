591663393c512c354397dde066e72177beb31e31
Fukuball Lin <fukuball@gmail.com>
2013-06-29 00:45:27 +0800

    ckip

--- fuku-class/private-param/ckip-param.php
+++ fuku-class/private-param/ckip-param.php
@@ -11,4 +11,10 @@
  * @version  Release: <1.0>
  * @link     http://www.fukuball.com
  */
+
+// ckip define
+define("CKIP_SERVER", "140.109.19.104");
+define("CKIP_PORT", 1501);
+define("CKIP_USERNAME", "tokenizer");
+define("CKIP_PASSWORD", "tokenizer123");
 ?>
