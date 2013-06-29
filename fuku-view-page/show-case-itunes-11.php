0b18cd53dc197f92a27ec3d2435b1387f53282ba
Fukuball Lin <fukuball@gmail.com>
2013-06-29 22:38:52 +0800

    js lib

---
+++ fuku-view-page/show-case-itunes-11.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * show-case-itunes-11.php
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-page/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+?>
+<script src="/public/javascript/library/expandingalbums/js/mustache.js"></script>
+<script src="/public/javascript/library/expandingalbums/js/jquery.app-folders.js"></script>
+<script src="/public/javascript/library/expandingalbums/js/quantize.js"></script>
+<script src="/public/javascript/library/expandingalbums/js/color-thief.js"></script>
