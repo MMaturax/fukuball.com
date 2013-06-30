0afcbf600d67dc33bcb948bf008762acf3ab4b5b
Fukuball Lin <fukuball@gmail.com>
2013-07-01 01:56:40 +0800

    ckip

--- fuku-view-component/meta-include.php
+++ fuku-view-component/meta-include.php
@@ -16,6 +16,15 @@ global $current_page_full_url;
 
 switch ($meta_type) {
 
+case 'ckip':
+   $og_title = $page_title;
+   $og_type = "website";
+   $og_url = $current_page_full_url;
+   $og_image = SITE_HOST."/public/image/avatar3.jpg";
+   $og_description = "自然語言處理系統最基本需要讓電腦能夠分辨文本中字詞的意義，才能夠更進一步發展出自然語言處理系統的相關演算法，其中斷詞處理便是一個重要的前置技術， CKIP Client 線上中文斷詞服務網站使用了中研院斷詞系統的 Client 端程式，讓有中文斷詞需求的研究者或程式人員可以專注於開發自己的核心演算法。";
+   $keywords = "fukuball, Fukuball Lin, 林志傑, CKIP-Client, CKIPClient-PHP, 中研院斷詞系統客戶端程式, 中文斷詞, 自然語言處理";
+   break;
+
 default:
    $og_title = $page_title;
    $og_type = "website";
