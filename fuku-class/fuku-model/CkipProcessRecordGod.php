8f051406f6ba9183b23e48b2bc6473a6e4a6c78b
Fukuball Lin <fukuball@gmail.com>
2013-07-09 00:54:51 +0800

    add model

---
+++ fuku-class/fuku-model/CkipProcessRecordGod.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * CkipProcessRecordGod.php is ckip_process_record god class
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-class/fuku-model/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+
+/**
+ * CkipProcessRecordGod is ckip_process_record god class
+ *
+ * An example of a CkipProcessRecordGod is:
+ *
+ * <code>
+ *   $ckip_process_record_god_obj = new CkipProcessRecordGod();
+ * </code>
+ *
+ * @category PHP
+ * @package  /fuku-class/fuku-model/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+class CkipProcessRecordGod extends ActiveRecordGod
+{
+   // extends from ActiveRecordGod
+   //
+   // protected $db_obj;
+   // protected $table_name;
+}
+?>
