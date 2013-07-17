395da23f3e2db3ee5dc10570cf390a21b8426632
Fukuball Lin <fukuball@gmail.com>
2013-07-17 18:30:58 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -13,17 +13,17 @@
  */
 ?>
 <section data-background="#000000">
-   <h1><span style="color: rgb(170, 136, 238);">5</span>Tunes11</h1>
+   <h1>5Tunes11</h1>
    <br/>
-   <h3>利用開源套件組合出 iTunes11 專輯設計</h3>
+   <h3>利用開源套件組出 iTunes11 專輯設計</h3>
    <br/>
-   <h3><span style="color: rgb(170, 136, 238);">Fukuball Lin</span></h3>
-   <br/>
-   <br/>
+   <h3>Fukuball Lin</h3>
    <br/>
    <br/>
    <br/>
-   <h4>HappyDesigner Mini 分享會 #3</h4>
+   <br/>
+   <br/>
+   <h4>HappyDesigner Mini 分享會 <span style="color: rgb(170, 136, 238);">#3</span></h4>
 </section>
 <section data-background="#000000">
    <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">iTunes 11</h2>
