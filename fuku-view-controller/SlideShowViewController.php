0ad387f48d9749baf6020477dea74bef665045ae
Fukuball Lin <fukuball@gmail.com>
2013-07-17 15:35:52 +0800

    slide

---
+++ fuku-view-controller/SlideShowViewController.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * SlideShowViewController.php is the controller
+ * to dispatch slide-show actions with show-case view
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-controller/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+
+/**
+ * SlideShowViewController.php is the controller
+ * to dispatch slide-show actions with slide-show view
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-controller/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+class SlideShowViewController
+   extends RESTControl
+      implements RESTfulInterface
+{
+
+   /**
+    * Dispatch post actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restPost($segments)
+   {
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new ErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restPost
+
+   /**
+    * Dispatch get actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restGet($segments)
+   {
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      case '5tunes11':
+
+         $page_title = '5tunes11 | Slide Show';
+         $yield_path = '/fuku-view-page/slide-show-5tunes11.php';
+         require_once SITE_ROOT.'/fuku-view-layout/show-case-page-layout.php';
+
+         break;
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new ErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restGet
+
+   /**
+    * Dispatch put actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restPut($segments)
+   {
+
+      $_PUT = array();
+      parse_str(file_get_contents('php://input'), $_PUT);
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new ErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restPut
+
+
+   /**
+    * Dispatch delete actions
+    *
+    * @param array $segments Method segments indicate action and resource
+    *
+    * @return void
+    */
+   public function restDelete($segments)
+   {
+
+      $_DELETE = array();
+      parse_str(file_get_contents('php://input'), $_DELETE);
+
+      $action_level_one_id = $segments[0];
+
+      switch ($action_level_one_id) {
+
+      default:
+         $type = 'page_not_found';
+         $parameter = array("none"=>"none");
+         $error_messanger = new ErrorMessenger($type, $parameter);
+         $error_messanger->printErrorJSON();
+         unset($error_messanger);
+         break;
+
+      }// end switch ($action_level_one_id)
+
+   }// end function restDelete
+
+}
+?>
---
+++ fuku-view-layout/slide-show-layout.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * slide-show-layout.php is the slide show layout
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /fuku-view-layout/
+ * @author   Fukuball Lin <fukuball@gmail.com>
+ * @license  Licence
+ * @version  Release: <1.0>
+ * @link     http://www.fukuball.com
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>Q2 Review &amp; Q3 Preview  - iNDIEVOX Engineering</title>
+      <?php
+      include_once SITE_ROOT."/fuku-view-component/meta-include.php";
+      include_once SITE_ROOT."/fuku-view-component/icon-include.php";
+      ?>
+      <link rel="stylesheet" href="/public/javascript/library/reveal/css/reveal.min.css">
+      <link rel="stylesheet" href="/public/javascript/library/reveal/css/theme/default.css" id="theme">
+      <!-- For syntax highlighting -->
+      <link rel="stylesheet" href="/public/javascript/library/reveal/lib/css/zenburn.css">
+      <!--[if lt IE 9]>
+         <script src="/public/javascript/library/reveal/lib/js/html5shiv.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+      <div class="reveal">
+         <!-- Any section element inside of this container is displayed as a slide -->
+         <div class="slides">
+            <?php
+            include_once SITE_ROOT.$yield_path;
+            ?>
+         </div>
+      </div>
+      <script src="/public/javascript/library/reveal/lib/js/head.min.js"></script>
+      <script src="/public/javascript/library/reveal/js/reveal.min.js"></script>
+      <script>
+      // Full list of configuration options available here:
+      // https://github.com/hakimel/reveal.js#configuration
+      Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+
+        theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+        transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+        // Optional libraries used to extend on reveal.js
+        dependencies: [
+          { src: '/public/javascript/library/reveal/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+          { src: '/public/javascript/library/reveal/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+          { src: '/public/javascript/library/reveal/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+          { src: '/public/javascript/library/reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+          { src: '/public/javascript/library/reveal/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+          { src: '/public/javascript/library/reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+        ]
+      });
+    </script>
+  </body>
+</html>
