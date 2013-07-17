835df6d1e05a4390a919d7853ce5ecdb6c52eb30
Fukuball Lin <fukuball@gmail.com>
2013-07-17 18:29:26 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -13,11 +13,11 @@
  */
 ?>
 <section data-background="#000000">
-   <h1>5Tunes11</h1>
+   <h1><span style="color: rgb(170, 136, 238);">5</span>Tunes11</h1>
    <br/>
-   <h3>利用開源套件組出 iTunes11 專輯設計</h3>
+   <h3>利用開源套件組合出 iTunes11 專輯設計</h3>
    <br/>
-   <h3>Fukuball Lin</h3>
+   <h3><span style="color: rgb(170, 136, 238);">Fukuball Lin</span></h3>
    <br/>
    <br/>
    <br/>
@@ -29,8 +29,8 @@
    <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">iTunes 11</h2>
    <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-transform: scaleX(0.9);">iTunes 11</h2>
    <img src="/public/image/slide-show/itunes-11-album-details.png" style="">
-   <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">Something Like This</h2>
-   <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-transform: scaleX(0.9);">Something Like This</h2>
+   <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">something like this</h2>
+   <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-transform: scaleX(0.9);">something like this</h2>
 </section>
 <section data-background="#000000">
    <h1>THE END</h1>
