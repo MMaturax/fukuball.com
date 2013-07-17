4eecfff00412aac58a0a2bbadd9c3ab60821f969
Fukuball Lin <fukuball@gmail.com>
2013-07-17 18:23:27 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -26,9 +26,11 @@
    <h4>HappyDesigner Mini 分享會 #3</h4>
 </section>
 <section data-background="#000000">
-   <h1 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">THIS IS SO</h1>
-   <h1 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-transform: scaleX(0.9);">THIS IS SO</h1>
+   <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">iTunes 11</h2>
+   <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; top: 7%; font-weight: bold; -webkit-transform: scaleX(0.9);">iTunes 11</h2>
    <img src="/public/image/slide-show/itunes-11-album-details.png" style="">
+   <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">Something Like This</h2>
+   <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; bottom: 5%; font-weight: bold; -webkit-transform: scaleX(0.9);">Something Like This</h2>
 </section>
 <section data-background="#000000">
    <h1>THE END</h1>
