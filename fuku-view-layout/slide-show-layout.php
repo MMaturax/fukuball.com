f835436f001a387ee4a88ce914f09ce10265824b
Fukuball Lin <fukuball@gmail.com>
2013-07-17 16:44:34 +0800

    slide

--- fuku-view-layout/slide-show-layout.php
+++ fuku-view-layout/slide-show-layout.php
@@ -72,6 +72,14 @@
           { src: '/public/javascript/library/reveal/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
         ]
       });
+
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-41911929-1', 'fukuball.com');
+        ga('send', 'pageview');
     </script>
   </body>
 </html>
