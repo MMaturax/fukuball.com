ce79d37bfb5d5761f62940ea7b5e5dd1efc8f1d1
Fukuball Lin <fukuball@gmail.com>
2013-07-19 13:55:11 +0800

    commercial

--- fuku-view-component/commercial/full-width-commercial.php
+++ fuku-view-component/commercial/full-width-commercial.php
@@ -12,13 +12,6 @@
  * @link     http://www.fukuball.com
  */
 ?>
-<!--<div id="full-width-commercial" class="commercial-box text-center">
-   <script async src="http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-   <ins class="adsbygoogle"
-        style="display:inline-block;width:728px;height:90px"
-        data-ad-client="ca-pub-9402461567510680"
-        data-ad-slot="7650184668"></ins>
-   <script>
-   (adsbygoogle = window.adsbygoogle || []).push({});
-   </script>
-</div>-->
+<!-- AdPlace ShowCode Begin / 960X100廣告 -->
+<iframe width="960" height="100" frameborder="0" scrolling="no" marginwidth="0" marginheight="0" src="http://adplace.adsame.com/s?z=adplace&c=1576&op=1"></iframe>
+<!-- AdPlace ShowCode End / 960X100廣告 -->
