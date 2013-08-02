dd070b7e4fde3cd76883ffa3136aca803305e017
Fukuball Lin <fukuball@gmail.com>
2013-08-03 00:11:20 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -25,8 +25,8 @@
    <br/>
    <h4>HappyDesigner Mini 分享會 <span style="color: rgb(170, 136, 238);">#3</span></h4>
 </section>
-<section data-background="#000000">
-   <h2 style="font-weight: bold; position: absolute; top: 1000%; color: rgb(0, 0, 0);">I am fukuball</h2>
+<section data-background="/public/image/slide-show/background.png">
+   <h2 style="font-weight: bold; position: absolute; top: 1000%;">I am fukuball</h2>
    <div style="font-weight: bold; position: absolute; top: 1200%; left: 0.5%;">
       CTO at <a href="http://indievox.com" target="_blank" style="height: 50px;" class="roll">iNDIEVOX</a>
    </div>
