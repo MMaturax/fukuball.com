81c339c66c42e91aadadb82056b320f2aa11060b
Fukuball Lin <fukuball@gmail.com>
2013-08-05 00:41:37 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -62,17 +62,6 @@
    <div class="mask-right">
       <img width="300px" height="300px" src="/public/image/itunes-cover/phptkWMy4480X480.jpg" style="margin:0px;border:none;max-width:100%;max-height:100%;" />
    </div>
-   <script>
-      $(document).ready(function() {
-
-         $(document.body).off('click.mask_action', '.mask-action');
-         $(document.body).on('click.mask_action', '.mask-action', function() {
-            $('.mask-left').addClass('mask-cover');
-            $('.mask-right').addClass('mask-cover');
-         });
-
-      });
-   </script>
 </section>
 <section data-background-color="rgba(50, 200, 90, 0.4)">
    <h3>
