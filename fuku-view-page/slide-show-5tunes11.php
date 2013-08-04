c5d38fd4f479703c4b3e78e1ea080c7e1a97aa14
Fukuball Lin <fukuball@gmail.com>
2013-08-04 23:57:59 +0800

    slide

--- fuku-view-page/slide-show-5tunes11.php
+++ fuku-view-page/slide-show-5tunes11.php
@@ -57,7 +57,8 @@
 <section data-background-color="rgba(255, 255, 255, 0.8)">
    <h2 class="absolute-element" style="position: absolute; z-index: 1; width: 100%; left: 0px; top: -700%; font-weight: bold; -webkit-text-stroke-width: 15px; -webkit-text-stroke-color: black; -webkit-transform: scaleX(0.9);">Create cover mask</h2>
    <h2 class="absolute-element" style="position: absolute; z-index: 2; width: 100%; left: 0px; top: -700%; font-weight: bold; -webkit-transform: scaleX(0.9);">Create cover mask</h2>
-
+   <div style="width:100px;height:100px;box-shadow: rgb(9, 8, 9) 14px 17px 25px inset, rgb(9, 8, 9) -1px -1px 170px inset;">
+   </div>
 </section>
 <section data-background-color="rgba(50, 200, 90, 0.4)">
    <h3>
