cd304810bfcf863777f1fee01e8546b8da6904f6
Fukuball Lin <fukuball@gmail.com>
2013-08-11 04:46:16 +0800

    debugging

--- fuku-view-page/show-case-color-quantization.php
+++ fuku-view-page/show-case-color-quantization.php
@@ -87,9 +87,20 @@
             var pixels     = imageData.data;
             var pixelCount = image.getPixelCount();
 
+            // Store the RGB values in an array format suitable for quantize function
+            var pixelArray = [];
+            for (var i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
+               offset = i * 4;
+               r = pixels[offset + 0];
+               g = pixels[offset + 1];
+               b = pixels[offset + 2];
+               a = pixels[offset + 3];
+               pixelArray.push([r, g, b]);
+            }
+
             // Send array to quantize function which clusters values
             // using median cut algorithm
-            var cmap    = MMCQ.quantize(pixels, colorCount);
+            var cmap    = MMCQ.quantize(pixelArray, colorCount);
             var palette = cmap.palette();
 
             // Clean up
