25860b81823708d1ea89ce8e0bf6a75cff42f310
Fukuball Lin <fukuball@gmail.com>
2014-06-11 16:46:10 +0800

    debugging

--- fuku-view-page/skrollr-demo.php
+++ fuku-view-page/skrollr-demo.php
@@ -51,10 +51,10 @@
       ?>
       <div id="bg-block">
       </div>
-      <div class="drangon-ball-char" style="top:300px;" data-0="right: -500px;" data-300="right: 300px;" data-600="opacity: 0;">
+      <div class="drangon-ball-char" style="top:300px;" data-0="opacity: 1;right: -500px;" data-300="opacity: 1;right: 300px;" data-600="opacity: 0;right: 400px;">
          <img src="/public/image/show-case/skrollr-demo/wukon.png" style="width:400px;">
       </div>
-      <div class="drangon-ball-char" style="top:600px;" data-300="left: -500px;" data-600="left: 300px;" data-900="opacity: 0;">
+      <div class="drangon-ball-char" style="top:600px;" data-300="opacity: 1;left: -500px;" data-600="opacity: 1;left: 300px;" data-900="opacity: 0;left: 400px;">
          <img src="/public/image/show-case/skrollr-demo/wukon3.png" style="width:400px;">
       </div>
       <script type="text/javascript">
