962b7f61676c4b52dc0cd3bf20fee31f4bb0744e
Fukuball Lin <fukuball@gmail.com>
2014-08-12 15:37:34 +0800

    debugging

--- fuku-view-page/show-case-drawer-drag.php
+++ fuku-view-page/show-case-drawer-drag.php
@@ -79,19 +79,9 @@
         <script src="/public/javascript/jquery.hammer/jquery.hammer-full.min.js"></script>
         <script>
 
-            document.body.addEventListener('touchmove', function(event) {
-      console.log(event.source);
-      //if (event.source == document.body)
-        event.preventDefault();
-    }, false);
-
-    window.onresize = function() {
-      $(document.body).width(window.innerWidth).height(window.innerHeight);
-    }
-
-    $(function() {
-      window.onresize();
-    });
+            document.ontouchmove = function(event){
+    event.preventDefault();
+}
 
             $(document).ready(function(){
 
