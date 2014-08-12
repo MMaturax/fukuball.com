c91ac891766e57eadf3529ba622f704d31d91d89
Fukuball Lin <fukuball@gmail.com>
2014-08-12 13:53:15 +0800

    add apple mobile

--- fuku-view-page/show-case-drawer-drag.php
+++ fuku-view-page/show-case-drawer-drag.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="description" content="Drawer Drag">
         <title>Drawer Drag</title>
