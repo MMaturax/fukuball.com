5e0017302bf4df0580c623a7b5dad2092f12afd6
Fukuball Lin <fukuball@gmail.com>
2015-07-07 16:16:04 +0800

    game

--- fuku-view-controller/GameViewController.php
+++ fuku-view-controller/GameViewController.php
@@ -106,6 +106,11 @@ class GameViewController
 
       case 'rock-paper-scissors':
 
+         file_get_contents("http://developers.facebook.com/tools/debug/og/object?q=http://www.fukuball.com/game/rock-paper-scissors&fbrefresh=89127348912");
+
+         $return_data
+            = exec('curl -X POST -F "id=http://www.fukuball.com/game/rock-paper-scissors" -F "scrape=true" "https://graph.facebook.com"');
+
          global $current_page_full_url;
 
          $rock_paper_scissors = array('rock', 'paper', 'scissors');
@@ -140,6 +145,7 @@ class GameViewController
             <body class="row-fluid p-a-0">
                <?php
                //include_once SITE_ROOT."/fuku-view-component/header/header.php";
+               var_dump($return_data);
                ?>
                <div class="text-center m-t-10">
                   <img src="/public/image/game/<?php echo $random_get; ?>.png" />
